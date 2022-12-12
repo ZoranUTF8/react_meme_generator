d340499ad64a3334264b500f7ff2de3437f95265
ZoranUTF8 <janjiczoran23@gmail.com>
2022-12-12 19:37:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { MainNavbar } from "./components/index";
+
+function App() {
+  return (
+    <div className="App">
+      <MainNavbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import MainNavbar from "./navbar/Navbar";
+
+export { MainNavbar };
